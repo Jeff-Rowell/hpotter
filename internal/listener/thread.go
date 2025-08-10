dd24d8928fca5f6af6e9562e1cf03394a88e83b2
Jeff Rowell <j3ff.rowell@gmail.com>
2025-08-11 05:27:28 +0800

    - updated thread.go to accept connections forever

--- internal/listener/thread.go
+++ internal/listener/thread.go
@@ -14,13 +14,17 @@ func Listen(service types.Service, wg *sync.WaitGroup) {
 	defer wg.Done()
 	lowerProto := strings.ToLower(service.ListenProto)
 	log.Printf("starting listener on %s port %d", lowerProto, service.ListenPort)
+
 	listenSocket, err := net.Listen(lowerProto, fmt.Sprintf("0.0.0.0:%d", service.ListenPort))
 	if err != nil {
 		log.Fatalf("error: failed to create listener: %v", err)
 	}
-	conn, err := listenSocket.Accept()
-	if err != nil {
-		log.Fatalf("error: failed to accept connection: %v", err)
+
+	for {
+		conn, err := listenSocket.Accept()
+		if err != nil {
+			log.Fatalf("error: failed to accept connection: %v", err)
+		}
+		log.Printf("connection received: (src=%s, dst=%s, proto=%s)", conn.RemoteAddr(), conn.LocalAddr(), conn.LocalAddr().Network())
 	}
-	log.Printf("connection received: (src=%s, dst=%s, proto=%s)", conn.RemoteAddr(), conn.LocalAddr(), conn.LocalAddr().Network())
 }
