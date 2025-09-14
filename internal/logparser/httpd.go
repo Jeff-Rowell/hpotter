eac2b5a4a574254db41a7e29ccc24af838daeb36
Jeff-Rowell <j3ff.rowell@gmail.com>
2025-09-13 19:10:43 -0600

    - added credential parsing for httpd

--- internal/logparser/httpd.go
+++ internal/logparser/httpd.go
@@ -5,6 +5,8 @@ import (
 	"regexp"
 	"strings"
 
+	"encoding/base64"
+
 	"github.com/Jeff-Rowell/hpotter/internal/database"
 	"github.com/Jeff-Rowell/hpotter/types"
 )
@@ -18,7 +20,7 @@ type HTTPDLogParser struct {
 func NewHttpdLogParser(service types.Service) *HTTPDLogParser {
 	parser := &HTTPDLogParser{
 		service:          service,
-		credentialRegex:  regexp.MustCompile(``),
+		credentialRegex:  regexp.MustCompile(`AUTH: Basic ([^"]{0,})`),
 		sessionDataRegex: regexp.MustCompile(`(\d{1,3}\.){3}\d{1,3} - (.*)`),
 	}
 
@@ -26,7 +28,28 @@ func NewHttpdLogParser(service types.Service) *HTTPDLogParser {
 }
 
 func (p *HTTPDLogParser) ParseCredentials(allLogData string) *database.Credentials {
-	// TODO
+	matches := p.credentialRegex.FindAllStringSubmatch(allLogData, -1)
+
+	for _, match := range matches {
+		if len(match) > 1 {
+			base64Decoded, err := base64.StdEncoding.DecodeString(match[1])
+			if err != nil {
+				log.Fatalf("error base64 decoding authorization header: %v", err)
+			}
+
+			decodedParts := strings.Split(string(base64Decoded), ":")
+
+			if len(decodedParts) < 2 {
+				log.Fatalln("error base64 decoding authorization header")
+			}
+
+			return &database.Credentials{
+				Username: decodedParts[0],
+				Password: decodedParts[1],
+			}
+		}
+	}
+
 	return nil
 }
 
@@ -35,7 +58,6 @@ func (p *HTTPDLogParser) ParseSessionData(allLogData string) *database.Data {
 	matches := p.sessionDataRegex.FindAllString(allLogData, -1)
 
 	for _, match := range matches {
-		log.Printf("DEBUG: match: %s", match)
 		if len(match) < 1 {
 			continue
 		}
